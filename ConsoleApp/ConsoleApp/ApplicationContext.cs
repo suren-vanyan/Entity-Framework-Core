a6a82126bdf66d020d69d2ba67c6a54bdc405bd7
suren-vanyan <vanyansuren@gmail.com>
2019-02-19 22:25:33 +0400

    Create, Read, Update, Delete

---
+++ ConsoleApp/ConsoleApp/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,76 @@
+﻿using ConsoleApp;
+using System;
+using System.Linq;
+
+namespace HelloApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Alice", Age = 26 };
+   
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+            }
+      
+            using (ApplicationContext db = new ApplicationContext())
+            {               
+                var users = db.Users.ToList();
+                
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+           
+            using (ApplicationContext db = new ApplicationContext())
+            {             
+                User user = db.Users.FirstOrDefault();
+                if (user != null)
+                {
+                    user.Name = "Bob";
+                    user.Age = 44;                  
+                    db.SaveChanges();
+                }
+                             
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+
+            
+            using (ApplicationContext db = new ApplicationContext())
+            {             
+                User user = db.Users.FirstOrDefault();
+                if (user != null)
+                {                   
+                    db.Users.Remove(user);
+                    db.SaveChanges();
+                }
+               
+               
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+
+            using (ApplicationContext context=new ApplicationContext())
+            {
+                var user1 = context.Users.FirstOrDefault();
+                var user2 = context.Users.LastOrDefault();
+                context.RemoveRange(user1, user2);
+                context.SaveChanges();
+            }
+            Console.Read();
+        }
+    }
+}
